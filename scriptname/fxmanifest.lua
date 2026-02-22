e7f69908aa85ee075fbc4bc670ab9f8d58b280f8
fordforlife2004-coder <fordforlife2004@gmail.com>
2026-02-22 13:33:05 -0600

    fxmanifest

--- scriptname/fxmanifest.lua
+++ scriptname/fxmanifest.lua
@@ -1,1 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
 
+author 'Preston'
+description 'Simple /hello command'
+version '1.0'
+
+client_script 'client.lua'
+RegisterCommand('hello', function()
+    TriggerEvent('chat:addMessage', { args = { '^2Hello from my FiveM portfolio!' } })
+end, false) 
